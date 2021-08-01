5be564b751c1ede437ce604f4beb8de1a77e4b4f
Cjorasia <jorasiac@gmail.com>
2021-08-01 12:06:10 +0530

    Dispatch, subscribe action for movies

--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,49 @@ import { data } from "../data";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favourites</div>
-        </div>
+class App extends React.Component {
 
-        <div className="list">
-          {data.map((movie, index) => (
-            <MovieCard movie={movie} key={`movie -${index}`} />
-          ))}
+  componentDidMount() {
+    // make API call
+    // dispatch action
+    const {store} = this.props;
 
+    // Subscription called after dispatch
+    store.subscribe(() => {
+      console.log('UPDATED');
+      this.forceUpdate(); // Never use this method
+    });
+
+    // Dispatch an action
+    store.dispatch({
+      type: 'ADD_MOVIES',
+      movies: data
+    });
+
+    console.log('STATE', this.props.store.getState());
+  }
+
+  render() {
+    const movies = this.props.store.getState(); // getting movies from store
+
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
+
+          <div className="list">
+            {data.map((movie, index) => (
+              <MovieCard movie={movie} key={`movie -${index}`} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
