df9a3d28121a14510cf510cde7a172cd77b6a740
Cjorasia <jorasiac@gmail.com>
2021-08-01 11:30:40 +0530

    redux store added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+// Package import
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from 'redux';
+
+// File import
 import "./index.css";
 import App from "./components/App";
+import movies from './reducers';
+
+// Creating redux store
+const store = createStore(movies);
+console.log("store", store);
 
 ReactDOM.render(<App />, document.getElementById("root"));
